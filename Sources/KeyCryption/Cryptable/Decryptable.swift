6e84e47a9c63f8f89c1b9197eac6be1087701a46
Noe Jupiter <noeonjupiter@icloud.com>
2022-05-15 19:31:00 +0300

    Fix

--- Sources/KeyCryption/Cryptable/Decryptable.swift
+++ Sources/KeyCryption/Cryptable/Decryptable.swift
@@ -9,12 +9,15 @@ import Foundation
 
 public protocol Decryptable {
     static func object(from: Data) -> Self?
-    static func decrypt(using key: String, data: Data) throws -> Self
+    static func decrypt(using key: String, data: Data?) throws -> Self
 }
 
 //MARK: - Decryptable & AES
 public extension Decryptable {
-    static func decrypt(using key: String, data: Data) throws -> Self {
+    static func decrypt(using key: String, data: Data?) throws -> Self {
+        guard let data = data else {
+            throw CryptionError.objectCreationFailed
+        }
         let decryptedData = try AES(key: key).decrypt(data)
         guard let object = object(from: decryptedData) else {
             throw CryptionError.objectCreationFailed
