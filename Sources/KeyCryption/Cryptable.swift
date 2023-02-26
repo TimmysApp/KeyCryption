059332e5db49a2e59743f1a0723ddfc24b0eee1a
Joe Maghzal <joe.maghzal@montymobile.com>
2023-02-26 15:25:30 +0200

    Protocols

--- Sources/KeyCryption/Cryptable.swift
+++ Sources/KeyCryption/Cryptable.swift
@@ -8,16 +8,19 @@
 import Foundation
 
 @propertyWrapper
-public class Cryptable<Value: Codable> {
+public class Cryptable<Value: Codable & Equatable & Hashable>: Codable {
+//MARK: - Properties
     private let key: String
     private let inMemory: Bool
     private var decryptedValue: Value?
     public var data = Data()
+//MARK: - Initializer
     public init(wrappedValue defaultValue: Value? = nil, key: CustomStringConvertible, inMemory: Bool = false) {
         self.key = key.description
         self.inMemory = inMemory
         self.decryptedValue = defaultValue
     }
+//MARK: - Mappings
     public var wrappedValue: Value {
         get {
             if let decryptedValue {
@@ -47,6 +50,7 @@ public class Cryptable<Value: Codable> {
     public var projectedValue: Cryptable<Value> {
         return self
     }
+//MARK: - Functions
     public func decrypt() {
         do {
             self.decryptedValue = try AES(key: key).decrypt(data, using: Value.self)
@@ -56,5 +60,19 @@ public class Cryptable<Value: Codable> {
     }
     public func removeFromMemory() {
         self.decryptedValue = nil
+    }
+}
+
+//MARK: - Hashable
+extension Cryptable: Hashable {
+    public func hash(into hasher: inout Hasher) {
+        hasher.combine(data)
+    }
+}
+
+//MARK: - Equatable
+extension Cryptable: Equatable {
+    public static func == (lhs: Cryptable<Value>, rhs: Cryptable<Value>) -> Bool {
+        return lhs.data == rhs.data
     }
 }
